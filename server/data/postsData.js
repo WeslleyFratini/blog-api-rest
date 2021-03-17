36a4a5e09a819a7e58fd9db87853a29dacf9d751
unknown <weeslleey354@gmail.com>
2021-03-17 20:30:39 -0300

    add teste save

--- server/data/postsData.js
+++ server/data/postsData.js
@@ -3,3 +3,14 @@ const database = require("../infra/database");
 exports.getPosts = function () {
   return database.query("select * from blog.post");
 };
+
+exports.savePost = function (posts) {
+  return database.one(
+    "insert into blog.post (title, content) values ($1, $2) returning *",
+    [posts.title, posts.content]
+  );
+};
+
+exports.deletePost = function (id) {
+  return database.nome("delete from blog.post where id = $1", [id]);
+};
--- server/test/posts.test.js
+++ server/test/posts.test.js
@@ -6,23 +6,36 @@ const generate = function () {
   return crypto.randomBytes(20).toString("hex");
 };
 
-test("Should get posts", async function () {
-  const post1 = postsService.savePost({
+const request = function (url, method, data) {
+  return axios({ url, method, data });
+};
+
+test.only("Should get posts", async function () {
+  const post1 = await postsService.savePost({
     title: generate(),
     content: generate(),
   });
-  const post2 = postsService.savePost({
+  const post2 = await postsService.savePost({
     title: generate(),
     content: generate(),
   });
-  const post3 = postsService.savePost({
+  const post3 = await postsService.savePost({
     title: generate(),
     content: generate(),
   });
-  const response = await axios({
-    url: "http://localhost:3000/posts",
-    method: "get",
-  });
+  const response = await request("http://localhost:3000/posts", "get");
   const posts = response.data;
   expect(posts).toHaveLength(3);
+  await postsService.deletePost(post1.id);
+  await postsService.deletePost(post2.id);
+  await postsService.deletePost(post3.id);
+});
+
+test.only("Should save posts", async function () {
+  const data = { title: generate(), content: generate() };
+  const response = await request("http://localhost:3000/posts", "posts", data);
+  const posts = response.data;
+  expect(post.title).toBe(data.title);
+  expect(post.content).toBe(data.content);
+  await postsService.deletePost(post1.id);
 });
